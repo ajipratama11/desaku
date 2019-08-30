c2e9aed1b018f06927eb6216361c86ebb2b460ca
Rendhy Pratama Putra <rendhypratama880@gmail.com>
2019-08-30 10:17:47 +0700

    ap

--- application/views/user/dashbord.php
+++ application/views/user/dashbord.php
@@ -560,11 +560,11 @@
       <div class="container">
         <div class="row">
           <div class="col-lg-9 text-center text-lg-left">
-            <h3 class="cta-title">Call To Action</h3>
-            <p class="cta-text"> Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <h3 class="cta-title">Informasi Update</h3>
+            <p class="cta-text"> Segala informasi terbaru terkait kepentingan dan berhubungan dengan kebutuhan masyarakat Desa Yosomulyo, Gambiran, Banyuwangi.</p>
           </div>
           <div class="col-lg-3 cta-btn-container text-center">
-            <a class="cta-btn align-middle" href="#">Call To Action</a>
+            <a class="cta-btn align-middle" href="#">Selengkapnya</a>
           </div>
         </div>
 
